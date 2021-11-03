4a767afab0f330345d4dd8387cad723645178124
yemmyharry <yemmyharry@gmail.com>
2021-11-03 21:58:56 +0100

    added context provider to render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { myProvider } from './context/context';
 
 ReactDOM.render(
   <React.StrictMode>
+    <myProvider>
     <App />
+    </myProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
