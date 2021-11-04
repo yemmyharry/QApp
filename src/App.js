f322aa4fef83bf2bf2ca489669366265383ae599
yemmyharry <yemmyharry@gmail.com>
2021-11-04 01:49:33 +0100

    added logic to switch components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useContext } from "react"
+import 'animate.css';
+import { MyContext } from "./context/context"
 import Initial from "./components/Initial"
 import Confirm from "./components/Confirm"
 import Results from "./components/Results"
@@ -5,10 +8,19 @@ import "./assets/App.css"
 
 
 const App = () => {
+  const context = useContext(MyContext)
+
+  const handleComponents = () => {
+
+    const screen = context.state.screen;
+        
+        if (screen === 0) return <Initial /> 
+        if (screen === 1) return <Confirm /> 
+        if (screen === 2) return  <Results />
+  }
   return (
     <div className="container">
-        <Initial />
-
+      {handleComponents()}
     </div>
   );
 }
