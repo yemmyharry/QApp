a955ac5354e9590fb1bb00c9611d34d825934f1d
yemmyharry <yemmyharry@gmail.com>
2021-11-04 01:50:49 +0100

    imported context and added function to change screen

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import {useContext} from 'react'
+import { MyContext } from '../context/context'
 
-export default function confirm() {
+
+export default function Confirm() {
+    const context = useContext(MyContext)
+
+    const nextScreen = () => {
+        context.changeScreen(2)
+    }
+
+    const firstScreen = () => {
+        context.changeScreen(0)
+    }
     return (
         <div>
-            confirm
+            <h3> Your Question Is: </h3>
+            <div className="viewer">
+                {context.state.question}
+            </div>
+            <div className="animate__animated animate__zoomIn animate__delay-1s">
+                <button className="btn" onClick={nextScreen} >Decide It</button>
+                <button className="btn" onClick={firstScreen} >Ask a new question</button>
+            </div>
         </div>
     )
 }
