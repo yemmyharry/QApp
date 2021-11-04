99881a028377fe18ba0eab9f1ebc921e4d95c88c
yemmyharry <yemmyharry@gmail.com>
2021-11-04 01:51:38 +0100

    used context in submit handler

--- src/components/Initial.js
+++ src/components/Initial.js
@@ -1,7 +1,8 @@
-import {React,useRef,useState} from 'react'
+import {React,useRef,useState, useContext} from 'react'
+import { MyContext } from '../context/context'
 
 export default function Initial() {
-
+    const context = useContext(MyContext)
     const textInput = useRef()
     const [showText, setShowBtn] = useState(false)
     const [showError, setShowError] = useState(false)
@@ -19,13 +20,16 @@ export default function Initial() {
             setShowError(true)
             return
         }
+
+        context.changeScreen(1)
+        context.question(value)
     }
 
     return (
         <div>
            <h1>Ask A Question</h1>
            <input ref={textInput} onChange={handleChange} type="text" className="form-control" name="question" />
-            {  showText ? (<button className="btn" onClick={handleSubmit} > Next </button>): <></>}
+            {  showText && <button className="btn animate__animated animate__fadeIn" onClick={handleSubmit} > Next </button>}
             {  showError ? (<p className="error" > Your question is too long. </p>): <></>}
            
         </div>
