b0c2fc715e20c8c5bac9042e8ce7cee895b56b2d
yemmyharry <yemmyharry@gmail.com>
2021-11-04 01:54:13 +0100

    added new function to navigate screens

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 
-
 const MyContext = React.createContext()
+
+let list = ['Call the Police', 'Sit down and relax', 'No answer for a such question', 'No', 'Yes', 'Maybe']
 
  class MyProvider extends Component {
      state = {
         screen: 0, 
-        question: ""
+        question: "",
+        result:""
      }
 
       changeNextScreen = (value) => {
@@ -21,13 +23,39 @@ const MyContext = React.createContext()
         })
      }
 
+     getRandomValue = () => {
+        return list[ Math.floor(Math.random()*list.length) ]
+     }
+
+     showResult = () => {
+         let decided = this.getRandomValue()
+         if (this.state.result !== "") {
+            while(decided === this.state.result){
+                decided = this.getRandomValue()
+            }
+         }
+         this.setState({
+             result: decided
+         })
+     }
+
+     resetHandler = () => {
+         this.setState({
+            screen: 0, 
+            question: "",
+            result:""
+         })
+     }
+
     render() {
         return (
             <div>
                 <MyContext.Provider value={{
                     state: this.state ,
                     changeScreen: this.changeNextScreen,
-                    question: this.showQuestion
+                    question: this.showQuestion,
+                    result: this.showResult,
+                    reset: this.resetHandler
                 }}>
                     {this.props.children}
                 </MyContext.Provider>
