d261ca2b66060bd8f1b9598cf896759c0a448638
yemmyharry <yemmyharry@gmail.com>
2021-11-04 01:54:53 +0100

    result component screen

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import {useContext, useEffect} from 'react'
+import { MyContext } from '../context/context'
 
-export default function results() {
+
+
+export default function Results() {
+
+const context = useContext(MyContext)
+
+useEffect(()=>{
+    context.result()
+},[])
     return (
         <div>
-            result
+            <h3> Your Answer Is: </h3>
+            <div className="viewer">
+                {context.state.result}
+            </div>
+            <div className="animate__animated animate__zoomIn animate__delay-1s">
+                <button className="btn" onClick={context.reset} > Start Over </button>
+                <button className="btn" onClick={context.result} > Decide Again </button>
+            </div>
         </div>
     )
 }
